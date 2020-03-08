b84d1a72b97c1a243230ba0083be36908f555143
MdReyajuddin <md.reyajuddin45@gmail.com>
2020-03-08 22:10:45 +0530

    blog1

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 
 from start.apps.blog.models import Entry
 from start.apps.blog.forms import EntryForm
+from start.apps.blog.decorators import user_is_entry_author
+
 
 # Create your views here.
 @login_required
@@ -25,6 +28,7 @@ def add(request):
     return render(request, 'blog/entry.html', {'form': form})
 
 @login_required
+@user_is_entry_author
 def edit(request, entry_id):
     entry=get_object_or_404(Entry, pk=entry_id)
     form = EntryForm(request.POST, instance=entry)
@@ -37,3 +41,21 @@ def edit(request, entry_id):
     return render(request, 'blog/entry.html', {'form': form})
 
 
+@login_required
+@user_is_entry_author
+def delete(request, entry_id):
+    entry=get_object_or_404(Entry, pk=entry_id)
+    entry.delete()
+    messages.success(request, 'Successfully Deleted')
+    return redirect('index')
+
+@login_required
+@user_is_entry_author
+def transfer_to(request, entry_id):
+    entry=get_object_or_404(Entry, pk=entry_id)
+    transfer_to=request.POST.get('transfer_to')
+    new_owner=User.objetcs.get(pk=transfer_to)
+    entry.created_by=new_owner
+    entry.save()
+    messages.success(request, 'Successfully Transfered')
+    return redirect('index')
